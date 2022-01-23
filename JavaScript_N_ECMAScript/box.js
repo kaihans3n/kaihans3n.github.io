45aeb39efb5e6860bfc5fb4eac1ae7fb338cb57c
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-01-23 08:41:02 +0700

    Update box.js

--- JavaScript_N_ECMAScript/box.js
+++ JavaScript_N_ECMAScript/box.js
@@ -1,3 +1,6 @@
+// binary, octal, hexadecimal list until 255
+
+
 var text = "<ol start='0'>";
 for (var i = 0; i <= 255; i++) {
   text += "<li>" + i.toString(2) + "</li>";
@@ -12,3 +15,11 @@ for (var j = 0; j <= 255; j++) {
 }
 text2 += "</ol>";
 document.getElementById("oct").innerHTML = text2;
+
+
+var text3 = "<ol start='0'>";
+for (var k = 0; k <= 255; k++) {
+  text3 += "<li>" + k.toString(16) + "</li>";
+}
+text3 += "</ol>";
+document.getElementById("hex").innerHTML = text3;
