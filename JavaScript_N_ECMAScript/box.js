f430f423fbc796ccce182f04ee39240260eebb22
Fedrik <52109641+kaihans3n@users.noreply.github.com>
2022-01-23 08:34:18 +0700

    Update box.js

--- JavaScript_N_ECMAScript/box.js
+++ JavaScript_N_ECMAScript/box.js
@@ -4,3 +4,11 @@ for (var i = 0; i <= 255; i++) {
 }
 text += "</ol>";
 document.getElementById("bin").innerHTML = text;
+
+
+var text2 = "<ol start='0'>";
+for (var j = 0; j <= 255; j++) {
+  text2 += "<li>" + j.toString(8) + "</li>";
+}
+text2 += "</ol>";
+document.getElementById("oct").innerHTML = text2;
